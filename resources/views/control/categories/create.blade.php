8f850d0af99430af33cf6f90d89922ba676a12cf
Dhuha-falah-92 <31364228+Dhuha-falah-92@users.noreply.github.com>
2019-03-18 15:38:28 +0300

    edit create

--- resources/views/control/categories/create.blade.php
+++ resources/views/control/categories/create.blade.php
@@ -36,7 +36,7 @@
                     </div>
     
                     <div class="form-group row">
-                        <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                        <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Details') }}</label>
     
                         <div class="col-md-6">
                             <input id="email" type="text" class="form-control{{ $errors->has('details') ? ' is-invalid' : '' }}" name="details" required>
