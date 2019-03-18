65d170b309f073bcc1fc287b42dffce1baf33143
Dhuha-falah-92 <dfalah92@gmail.com>
2019-03-18 09:04:13 +0300

    app

--- config/database.php
+++ config/database.php
@@ -43,7 +43,7 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'host' => '127.0.0.1',
-            'port' => '3307',
+            'port' => '3306',
             'database' => 'test',
             'username' =>  'root',
             'password' =>  '',
